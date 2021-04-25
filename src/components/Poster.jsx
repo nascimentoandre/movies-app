0f24f623b46a72db922a243ab878c0ed0c4d68e3
André Nascimento <andrevsn1@gmail.com>
2021-04-25 11:10:30 -0300

    component for a movie poster

---
+++ src/components/Poster.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Poster = () => {
+  return (
+    <View>
+      <Text>poster</Text>
+    </View>
+  );
+}
+
+export default Poster;
