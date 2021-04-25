a235894ce86db2d8caba8f477bc5199447220520
André Nascimento <andrevsn1@gmail.com>
2021-04-24 22:10:24 -0300

    wrap theme provider around application

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/configureStore";
 import Home from "./src/screens/Home";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
 
 export default function App() {
+
   return (
     <>
       <Provider store={store}>
-        <Home />
-        <StatusBar />
+        <ThemeProvider theme={theme}>
+          <Home />
+          <StatusBar />
+        </ThemeProvider>
       </Provider>
     </>
   );
