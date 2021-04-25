e8f66b4b91a10930bb2e47de2d39ce290dfeae29
André Nascimento <andrevsn1@gmail.com>
2021-04-24 22:08:57 -0300

    add popular reducer logic

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import movieReducer from "./ducks/movie";
+import popularReducer from "./ducks/popular";
 import { watcherSaga } from "./sagas/rootSaga";
 
 const reducer = combineReducers({
-  movie: movieReducer
+  movie: movieReducer,
+  popular: popularReducer
 });
 
 const sagaMiddleware = createSagaMiddleware();
--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,7 +1,10 @@
 import { takeLatest } from "redux-saga/effects";
 import { handleGetMovie } from "./handlers/movie";
 import { GET_MOVIE } from "../ducks/movie";
+import { handleGetPopular } from "./handlers/popular";
+import { GET_POPULAR } from "../ducks/popular";
 
 export function* watcherSaga() {
   yield takeLatest(GET_MOVIE, handleGetMovie);
+  yield takeLatest(GET_POPULAR, handleGetPopular);
 }
