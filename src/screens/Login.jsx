cb36d89d42ad5f0434f6b533018b49f03e120220
André Nascimento <andrevsn1@gmail.com>
2021-04-25 20:59:41 -0300

    edit button

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Loading from "../components/Loading";
+import { ImgBg, LoginButton, LoginText } from "../styles/screens/login";
 
-const Login = () => {
+const Login = ({ navigation }) => {
+  const bg_img = require("../assets/background.jpeg");
+
+  if (!bg_img) return <Loading />
+
   return (
-
-  )
+    <ImgBg imageStyle={{opacity: 0.8}} source={bg_img}>
+      <LoginButton onPress={() => navigation.navigate("Home")}>
+        <LoginText>Entrar como visitante</LoginText>
+      </LoginButton>
+    </ImgBg>
+  );
 }
 
 export default Login;
