e0f8de79c5ec7aa0ad11d38996e97c831a4df95f
André Nascimento <andrevsn1@gmail.com>
2021-04-25 12:16:41 -0300

    export new styles

--- src/styles/screens/home.js
+++ src/styles/screens/home.js
@@ -14,8 +14,25 @@ export const OptionsContainer = styled.View`
   justify-content: space-around;
   flex-direction: row;
   width: 100%;
+  margin-bottom: 20px;
 `
 export const Title = styled.Text`
   color: ${props => props.theme.colors.text};
   font-size: 20px;
+`
+
+export const PosterStyle = styled.Image`
+  width: 150px;
+  height: 230px;
+  border-radius: 15px;
+  margin-bottom: 8px;
+`
+
+export const PosterContainer = styled.View`
+  flex: 0.8;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 1px solid white;
 `
