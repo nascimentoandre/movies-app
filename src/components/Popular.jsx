cc3c5103a7bea004cd5ec61bac5b8fb33506bf32
André Nascimento <andrevsn1@gmail.com>
2021-04-24 22:12:08 -0300

    create new components

---
+++ src/components/Popular.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Popular = () => {
+  return (
+    <>
+      <Text style={{fontSize: 35}}>pop</Text>
+    </>
+  );
+}
+
+export default Popular;
