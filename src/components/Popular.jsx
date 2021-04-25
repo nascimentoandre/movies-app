c42db3a6ad57dd4858c1ad1e66643ff02c5616e9
André Nascimento <andrevsn1@gmail.com>
2021-04-25 12:14:38 -0300

    add a few movies to test

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import { Text } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { getPopular } from "../redux/ducks/popular";
+import Loading from "./Loading";
 import Poster from "./Poster";
 
 const Popular = () => {
+  // importando dados
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,10 +15,16 @@ const Popular = () => {
 
   const popular = useSelector(state => state.popular.popular);
 
+  // Tela de carregamento
+  if (!popular) return <Loading />
+
+  // Posters dos filmes
   return (
     <>
-      <Text style={{fontSize: 35}}>pop</Text>
-      {popular && <Text>{popular.results[0].poster_path}</Text>}
+      <Poster posterPath={popular.results[0].poster_path}/>
+      <Poster posterPath={popular.results[1].poster_path}/>
+      <Poster posterPath={popular.results[2].poster_path}/>
+      <Poster posterPath={popular.results[3].poster_path}/>
     </>
   );
 }
