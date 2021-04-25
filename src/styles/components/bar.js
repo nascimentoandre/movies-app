5a4c80770d811045b4dd715d998c363b70f51070
André Nascimento <andrevsn1@gmail.com>
2021-04-25 14:06:26 -0300

    bottom bar style

---
+++ src/styles/components/bar.js
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const BarStyle = styled.View`
+  height: 8%;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  background-color: ${props => props.theme.colors.bar};
+  margin-top: 10px;
+`
+
+export const Item = styled.TouchableHighlight`
+  color: ${props => props.theme.colors.icon2};
+`
