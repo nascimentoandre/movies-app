89423b4ca1dc1edaa47e44b12f42fd8b6662781e
André Nascimento <andrevsn1@gmail.com>
2021-04-26 13:55:02 -0300

    finish basic detail layout

--- src/screens/Movie.jsx
+++ src/screens/Movie.jsx
@@ -1,24 +1,52 @@
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { getTop } from "../redux/ducks/top";
+import { getMovie } from "../redux/ducks/movie";
+import { DetailImage, MovieContainer, Info, InfoText, Title } from "../styles/screens/movie";
+import Loading from "../components/Loading";
+import BottomBar from "../components/BottomBar";
+import ImgBg from "../styles/screens/login";
+import { ScrollView } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Movie = ({ route }) => {
   const dispatch = useDispatch();
 
+  id = route.params.id;
+
   useEffect(() => {
-    dispatch(getTop());
+    dispatch(getMovie(id));
   }, [dispatch]);
 
-  const top = useSelector(state => state.top.top);
+  const movie = useSelector(state => state.movie.movie);
+
+  if (!movie) return <Loading />
+
+  const path = "https://image.tmdb.org/t/p/w500/" + movie.poster_path; 
+  const genres = [];
+  for (let i in movie.genres) genres.push(movie.genres[i].name);
 
   return (
-    <View>
-      <Text>Hello from movie</Text>
-      <Text>ID: {route.params?.id} </Text>
-      {top && <Text>{top.results[0].title}</Text>}
-    </View>
+    <MovieContainer>
+      <ScrollView contentContainerStyle={scrollStyle} scrollEnabled={true}>
+        <DetailImage source={{uri: path}}/>
+        <Info>
+          <Title>{movie.title}</Title>
+          <InfoText>{movie.release_date.slice(0,4)} • {genres.join("/")} • {movie.vote_average}/10  <FontAwesomeIcon icon={faStar} color={"#f52be0"}/></InfoText>
+          <InfoText>{movie.overview}</InfoText>
+        </Info>
+      </ScrollView>
+      <BottomBar />
+    </MovieContainer>
   );
+}
+
+const scrollStyle = {
+  alignItems: "center",
+  justifyContent: "space-around",
+  width: "100%",
+  flexDirection: "row",
+  flexWrap: "wrap",
 }
 
 export default Movie;
